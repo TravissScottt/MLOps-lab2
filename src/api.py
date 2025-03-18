330e8a37d655178beca05023da2811f142029c46
Dolmachi <ernestkuznecov8@gmail.com>
2025-03-18 10:10:08 +0300

    add database in api

--- src/api.py
+++ src/api.py
@@ -3,17 +3,23 @@ from pydantic import BaseModel
 import pandas as pd
 import os
 import sys
+from datetime import datetime
 
 sys.path.insert(0, os.path.join(os.getcwd(), "src"))
 
 from predict import PipelinePredictor
+from database import get_database
+from logger import Logger
 
-# Инициализируем предиктор
+
+SHOW_LOG = True
+logger = Logger(SHOW_LOG).get_logger(__name__)
 predictor = PipelinePredictor()
+db = get_database()
 
 app = FastAPI()
 
-# Определяем формат данных для запроса
+# Определяем модель входных данных
 class CarFeatures(BaseModel):
     Doors: int
     Year: int
@@ -24,13 +30,26 @@ class CarFeatures(BaseModel):
     Transmission: str
     Engine_Size: float
     Mileage: float
-    
 
 @app.post("/predict")
-def predict(features: CarFeatures):
-    # Принимаем данные и преобразуем в DataFrame
+def predict_api(features: CarFeatures):
+    # Преобразуем входные данные в DataFrame для предсказания
     input_data = pd.DataFrame([features.model_dump()])
-    # Предсказываем цену с предиктора
     prediction = predictor.predict(input_data)
     
+    # Подготовка данных для сохранения в MongoDB
+    result_data = {
+         "input": features.model_dump(),
+         "prediction": prediction[0],
+         "timestamp": datetime.utcnow().isoformat()
+    }
+    
+    # Сохраняем результат в коллекцию 'predictions'
+    try:
+        result = db.predictions.insert_one(result_data)
+        logger.info(f"Prediction saved with id: {result.inserted_id}")
+    except Exception as e:
+        logger.error("Error saving prediction", exc_info=True)
+    
     return {"prediction": prediction[0]}
+
